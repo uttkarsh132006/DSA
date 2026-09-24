6c0a4f6d2cea0c5389f12b943ed15bb5aae392a2
uttkarsh132006 <uttkarsh56789@gmail.com>
2026-09-24 23:26:22 +0530

    add the complete soln

--- binary_tree/nodesAtK.cpp
+++ binary_tree/nodesAtK.cpp
@@ -14,26 +14,26 @@ struct TreeNode {
 
 
  //this is my soln not a complete one i will pass it two time to make sure every step is connected
-void kin(TreeNode* root,TreeNode* target,int &k,vector<int>&ans,int &now){
-    if(!root){
-        now--;
-        return ;
-    }
+// void kin(TreeNode* root,TreeNode* target,int &k,vector<int>&ans,int &now){
+//     if(!root){
+//         now--;
+//         return ;
+//     }
 
-    if(root==target)now=0;
+//     if(root==target)now=0;
 
-    if (find(ans.begin(), ans.end(), root->val) == ans.end() && k==now) {
-        ans.push_back(root->val);
-    }
+//     if (find(ans.begin(), ans.end(), root->val) == ans.end() && k==now) {
+//         ans.push_back(root->val);
+//     }
     
-    now++;
-    kin(root->left,target,k,ans,now);
-    now++;
-    kin(root->right,target,k,ans,now);
+//     now++;
+//     kin(root->left,target,k,ans,now);
+//     now++;
+//     kin(root->right,target,k,ans,now);
 
     
-    now--;
-}
+//     now--;
+// }
 
 //orignal
 void parentMapi(TreeNode* root,unordered_map<TreeNode*,TreeNode*>& mpp){
@@ -62,46 +62,53 @@ void parentMapi(TreeNode* root,unordered_map<TreeNode*,TreeNode*>& mpp){
 vector<int> maini(TreeNode* root,TreeNode* target,int k,unordered_map<TreeNode*,TreeNode*>& parentMap){
     unordered_set<TreeNode*>visited;
     vector<int>ans;
-    if(target){
-        visited.insert(target);
-    }
+    
+    visited.insert(target);
+    
 
     int current=0;
 
     queue<TreeNode*>q;
     q.push(target);
+    int flag=0;
 
     while(!q.empty()){
         int siz=q.size();
-        int flag=0;
-        if(current++==k){
+        if(current==k){
             flag=1;
         }
         for(int i=0;i<siz;i++){
             root=q.front();
             q.pop();
-
+            
             if(root->left&&visited.find(root->left)==visited.end()){
                 q.push(root->left);
-                if(flag){
-                    ans.push_back(root->left->val);
-                }
+                visited.insert(root->left);
+                // if(flag){
+                //     ans.push_back(root->left->val);
+                // }
             }
             if(root->right&&visited.find(root->right)==visited.end()){
                 q.push(root->right);
-                if(flag){
-                    ans.push_back(root->right->val);
-                }
+                visited.insert(root->right);
+                // if(flag){
+                //     ans.push_back(root->right->val);
+                // }
             }
             if(parentMap[root]&&visited.find(parentMap[root])==visited.end()){
                 q.push(parentMap[root]);
-                if(flag){
-                    ans.push_back(parentMap[root]->val);
-                }
+                visited.insert(parentMap[root]);
+                // if(flag){
+                //     ans.push_back(parentMap[root]->val);
+                // }
             }
-
+            if(flag){
+                ans.push_back(root->val);
+            }
+            
         }
         if(flag)break;
+        current++;
 
     }
     return ans;
